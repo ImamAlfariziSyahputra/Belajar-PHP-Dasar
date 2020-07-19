a0ecde13fb891bb76c82058cd55934c19b002904
Imam Alfarizi Syahputra <63343105+ImamAlfariziSyahputra@users.noreply.github.com>
2020-07-19 17:42:13 +0700

    Menambahkan Fitur Session
    - jika belum login tidak bisa masuk ke halaman index.php
    - jika sudah login tidak bisa kembali ke halaman Login (Logout terlebih dahulu)

--- pertemuan13/login.php
+++ pertemuan13/login.php
@@ -1,4 +1,11 @@
 <?php 
+session_start();
+
+if(isset($_SESSION["login"])) {
+    header("Location: index.php");
+    exit;
+}
+
 require 'functions.php';
 
 if(isset($_POST["login"])) {
@@ -13,6 +20,9 @@ if(isset($_POST["login"])) {
         // cek password
         $row = mysqli_fetch_assoc($result);
         if(password_verify($password, $row["password"])) {
+            // set SESSION
+            $_SESSION["login"] = true;
+
             header("location: index.php");
             exit;
         }
